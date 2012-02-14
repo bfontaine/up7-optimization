eed6f4a7cb1461fb3a32c87ee3e7b29b1e0ce5ae
Baptiste Fontaine <batifon@yahoo.fr>
2012-02-14 12:40:53 +0100

    Some files from the original website added

---
+++ js/jquery.diaporama3.js
@@ -0,0 +1,248 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html lang="fr">
+<HEAD>
+<!--<link href="/site_courant/fckeditor.css" rel="stylesheet" type="text/css">-->
+<link href="/sc/commun.css" rel="stylesheet" type="text/css">
+<link href="site.css" rel="stylesheet" type="text/css">
+<TITLE>"Erreur 404 - Universite Paris Diderot (Paris 7)</TITLE>
+<META http-equiv=Content-Type content="text/html; charset=iso-8859-1">
+<meta http-equiv="Content-Style-Type" content="text/css">
+
+
+</HEAD>
+
+<body bgcolor="#FFFFFF">
+<center>
+<div class="bg_droite">
+<div class="bg_gauche">
+<div id="page1">
+<table width="1024" cellpadding="0" cellspacing="0"  background="../sc/images/bande_Rouge.jpg" class="enteteTable">
+	<tr>
+		<td width="150"  rowspan="2"><a href="../index.php">
+<img src="../sc/images/Logo_UPD_H_R.png" border="0" align="top" alt="Université Paris Diderot Paris 7"></a></td>
+		<td><img src="../accueil-P7.png" alt="Batiments de l'universit&eacute; Paris Diderot" width="874" height="107"border="0" usemap="#Map0"></td>
+
+	</tr>
+	<tr>
+		<td></td>
+	</tr>
+</table>
+<map name="Map0">
+<area shape="rect" coords="119,88,227,109" href="../formation/index.php?g=m&numpage=0">
+<area shape="rect" coords="228,90,390,119" href="../sc/site.php?bc=vietudiante&np=accueil&g=m">
+<area shape="rect" coords="392,89,509,115" href="../recherche/recherche.php?g=m">
+<area shape="rect" coords="508,89,612,112" href="../sc/site.php?bc=international&np=ACCUEIL&g=m">
+<area shape="rect" coords="766,86,863,115" href="http://portail.univ-paris-diderot.fr/render.userLayoutRootNode.uP"><area shape="rect" coords="-1,88,116,107" href="../sc/site.php?bc=universite&np=accueil&g=m">
+</map>
+<div id="fondDroit">
+<div id="menuDroit">
+
+
+
+<ul>
+<div class="menuDroitLu">Espaces : </div> 
+<li><a href="../entreprise.php">ENTREPRISE</a></li>
+<li><a href="../personnel.php">PERSONNEL</a></li>
+<li><a href="./communiques/index.php">PRESSE</a></li>
+</ul><br>
+<br>
+<div align="center"></div>
+<ul>
+        <a href="images/BDSciences.pdf"><img src="images/objectifScience.png" alt="objectif sciences"  border="0" /></a>
+        <font face="Verdana" color="#333399">
+        <b><font size="2"></font></b></font></td>
+</ul>
+<br /><br>
+<br /><br>
+<div align="center">
+<a href="http://www.univ-paris-diderot.fr/guides/guide_etudiant/index.html" target="_blank"> 
+<img src="/guides/guide_etudiant/datas/jpgmini/page-0001.jpg" width="121" height="85" border="0" alt="Guide de l'étudiant"></a><br />
+<span class="Style1" >Guide de l'étudiante<br /> et de l'étudiant<br />Année 2011-2012</span></div>
+<br />
+<div align="center"><a href="http://www.univ-paris-diderot.fr/guides/guide_associations/index.html" target="_blank"> 
+<img src="/guides/guide_associations/datas/jpgmini/page-0001.jpg" width="107" height="106" border="0" alt="Guide des associations"></a><br />
+<span class="Style1" >Guide des associations<br />
+et de la vie &eacute;tudiante
+<br />
+Ann&eacute;e 2011-2012</span></div>
+
+</div>
+<div id="fondGauche">
+<div id="navigation" > <a href="/index.php">
+<img border="0" align="top" src="/sc/images/Logo_UPD_B_R.png"></a>
+<br><div class="menuGauche">
+
+<div style="margin-left:10px">
+<a href="../../index.php" >
+<img src="/DocumentsFCK/accueil/Image/drapeau_francais.jpg"  border="0" alt="french" /></a>
+<a href="/english/sc/site.php?bc=accueil&np=accueil&g=m/" ><img src="/DocumentsFCK/accueil/Image/uk.png"  border="0" alt="english" /></a>
+<a href="/spanish/sc/site.php?bc=accueil&np=accueil&g=m/" ><img src="/DocumentsFCK/accueil/Image/spain.png"  border="0" alt="spanish" /></a>
+<a href="/chinese/sc/site.php?bc=accueil&np=accueil&g=m/" ><img src="/DocumentsFCK/accueil/Image/china.png"  border="0" alt="chinese" /></a>
+</div>
+
+
+<ul><li><a href="http://www.univ-paris-diderot.fr/nouscontacter.php">Nous contacter</a></li></ul>
+<ul><li><a href="/sc/site.php?bc=implantations&amp;np=ACCUEIL&g=m">Venir</a></li></ul>
+<ul>
+  <li><a href="http://www.univ-paris-diderot.fr/sc/site.php?bc=inscriptions&np=ACCUEIL&g=m">Inscriptions/Scolarit&eacute;</a></li></ul>
+<!--<ul><li><a href="/sc/site.php?bc=PRG&amp;np=ACCUEIL&g=m">Infos Paris Rive gauche</a></li></ul>-->
+<ul><li><a href="/LDAP/">ANNUAIRE</a></li></ul>
+
+<ul><li>&nbsp;</li>
+</ul>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a  class="ligne" href="http://fr-fr.facebook.com/univparisdiderot" title="page FaceBook Paris Diderot"><img src="/images/faceBook.png" alt="FaceBook" border ="0" align="middle"   /></a>
+<a  class="ligne" href="http://twitter.com/#ParisDiderot" title="Twitter">
+<img src="/images/twitter.png" alt="Fil Rss du site" border ="0" align="middle"   /></a>
+
+<a  class="ligne" href="http://www.univ-paris-diderot.fr/universite/rssActu1.php" title="Syndiquer tout le site">
+<img src="/images/feed.png" alt="Fil Rss du site" border ="0" align="middle"   /></a>
+
+
+<br />
+
+
+
+<!-- Debut GSA form -->
+<form name="form1" method="get" action="http://cherche.univ-paris-diderot.fr/search">	
+<div class="rechform">
+<div class="titrerech"> Rechercher  </div>
+<div class="boutonRech">
+	
+		
+        <br>
+      <input name="q" class="saisieRech" size="20" maxlength="100" type="text">	
+		<input type="hidden" name="client" value="default_frontend" />
+		<input type="hidden" name="output" value="xml_no_dtd" />
+		<input type="hidden" name="filter" value="0" />
+		<input type="hidden" name="proxystylesheet" value="default_frontend" />
+		
+        <input name="Submit" type="submit" class="clickRech" value="&nbsp;">
+       
+		 
+         <div ><br><SELECT class="saisieRechSelect" NAME="site">  
+	  	<OPTION VALUE="Tout">Dans tout le site</OPTION>
+        <OPTION VALUE="Formations">Formations</OPTION>
+        <OPTION VALUE="Laboratoires">Laboratoires</OPTION>
+        </SELECT></div>
+        
+        
+        
+        
+</div>
+</div><br>
+</form>
+<!-- Fin GSA form -->
+
+
+<br /><br /><br /><br />
+<!--<div class="encadre">
+<a href="/sc/site.php?bc=recrutement&np=ACCUEIL&g=m" ><img src="/recrutement/images/stickerrecrutement.png" width="140" height="172" vspace="10" border="0" /></a></div>
+-->
+<div id="cleardiv"></div>
+<br />
+<div class="encadre"><p align="center">
+<a href="http://www.sorbonne-paris-cite.fr/" target="_blank"><img src="/images/logo-Sorbonne-Paris-Cite.png"  border="1" alt="logo du pres"/></a></p>
+</div>
+<br />
+<div class="encadre">
+<p align="center">
+<a href="/sc/site.php?bc=recrutement&np=ACCUEIL&g=m" ><img src="/recrutement/images/stickerrecrutement_3.png"  border="1" alt="recrutement" /></a>
+</div>
+<div id="cleardiv"></div><br>
+</div>    
+</div>
+
+<div id="content">
+<div class="bas">
+			   <div class="titreP7-2" ></div>
+  
+ 
+    <div><b><span style="font-size: 10.0pt; color: black; font-weight: bold">L'adresse 
+    demandée ne correspond pas à une page de&nbsp;notre serveur</span></b><br>
+    <span lang="EN-GB" style="font-size: 10.0pt; color: red">The requested URL was not found on this server</span></div>
+<div  class="titreP7-2"><b>Vous pouvez chercher : </b></div>
+
+
+<div  class="titreActuFlash">un laboratoire </div> 
+
+<form name="form1" method="post" action="/recherche/findlabo4.php">
+<fieldset class="libsaisief">	 
+(Par intitulé, directeur, numéro ou mot-clef)<br>			  		  			  
+  <input name="nom_rech" type="text"  id="nom_rech2" size="20" maxlength="120">         
+  <input name="Submit" type="submit"  value="Rechercher">
+</fieldset>
+</form> 
+<div  class="titreActuFlash">une formation </div>
+<form name="form1" method="post" action="http://www.univ-paris-diderot.fr/sc/site.php?bc=formations&np=FINDFORM">	    
+<fieldset class="libsaisief">
+<input name="critere" type="radio" value="un" checked> 
+Sur un des mots<br>
+<input type="radio" name="critere" value="tous"> 
+Sur l'expression exacte<br>
+
+<input name="nom_mention" type="text" class="texte" size="20" maxlength="100">
+<input name="Submit" type="submit" class="bouton" value="Rechercher">
+</fieldset>
+</form>
+
+<div  class="titreActuFlash">une personne </div> 	 
+<p><a href="/LDAP/" class="liste">Annuaire public </a>
+
+<p><a href="/IntraP7/LDAP/index.php" class="liste">Annuaire dans l'intranet</a> </p>
+
+    </p>
+<div  class="titreActuFlash">Recherche sur tout le site de l'université</div> 	
+	
+	
+	
+    <form method="post" action="/cgi-bin/htsearch">
+	<fieldset class="libsaisief">
+      <input type="hidden" name="config" value="htdig"><input type="hidden" name="restrict" value>
+	  <input type="hidden" name="exclude" value>
+      Résultat		
+	  <select name="method" size="1">
+        <option value="and">Tous les mots clés</option>
+        <option value="or">Au moins un des mots</option>
+      </select> 
+	  
+	   Format&nbsp;&nbsp;
+	   <select name="format" size="1">
+        <option value="builtin-long">Long</option>
+        <option value="builtin-short">Court</option>
+      </select>
+	 Tri&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  <select name="sort" size="1">
+        <option value="score">Score</option>
+        <option value="time">Date</option>
+        <option value="title">Titre</option>
+        <option value="revscore">Score tri inversé</option>
+        <option value="revtime">Date tri inversé</option>
+        <option value="revtitle">Titre tri inversé</option>
+      </select>  <br><br>
+     
+      
+	  <input type="text" size="30" name="words" value>
+	  <input type="submit" value="Rechercher">
+    </fieldset>
+	</form>
+    <div  class="titreActuFlash">Recherche manuelle sur le site de l'université</div> 	 
+<a href="/cartesite.php" class="liste">Plan du site</a>
+	
+  </div>
+<div id="cleardiv"></div>
+</div>
+<div id="footer">
+<div id="header"><div id="menu_footer">
+<a href="site.php?bc=annuaire&np=ACCUEIL">Annuaire</a>
+<a href="site.php?bc=accueil&np=contact&g=sm">Contact</a>
+<a href="site.php?bc=implantations&np=ACCUEIL">Venir</a>
+<a href="site.php?bc=accueil&np=plan_site&g=sm">Plan du site</a>
+<a href="https://cas.sc.univ-paris-diderot.fr/index.jsp?service=http://portail.univ-paris-diderot.fr/Login">Portail ENT/Intranet</a>
+<a href="site.php?bc=accueil&np=mentions_legales&g=sm#">Informations l&eacute;gales</a>
+<a href="mailto:communic@univ-paris-diderot.fr">Universit&eacute; Paris Diderot - 2012</a>
+</div>
+</div> 
+</div></center>
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+	$(".diaporama").diaporama({
+		animationSpeed: "slow",
+		delay:2
+	});
+
+});
+$(document).ready(function(){
+
+	$(".diaporama1").diaporama({
+		animationSpeed: "slow",
+		delay:2,
+		controls:false
+	});
+
+});
+$(document).ready(function(){
+
+	$(".diaporama2").diaporama({
+		animationSpeed: "slow",
+		delay:2.5,
+		controls:false
+	});
+
+});
+
+
